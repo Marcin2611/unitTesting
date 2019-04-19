493865ca13261c1882974aa3754ecb439574a90d
Marcin <marcin.golec1995@gmail.com>
2019-04-19 09:15:04 +0200

    Annotations @BeforeEach, @BeforeAll, @AfterEach, @AfterAll

--- src/main/java/pl/devfoundry/testing/Meal.java
+++ src/main/java/pl/devfoundry/testing/Meal.java
@@ -27,6 +27,14 @@ public class Meal {
     }
 
     @Override
+    public String toString() {
+        return "Meal{" +
+                "price=" + price +
+                ", name='" + name + '\'' +
+                '}';
+    }
+
+    @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
---
+++ src/test/java/pl/devfoundry/testing/OrderBackupTest.java
@@ -0,0 +1,49 @@
+package pl.devfoundry.testing;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+class OrderBackupTest {
+
+    private static OrderBackup orderBackup;
+
+    @BeforeAll
+    static void setUp() throws FileNotFoundException {
+        orderBackup = new OrderBackup();
+        orderBackup.createFile();
+    }
+
+    @BeforeEach
+    void appendAtTheStartOfTheLine() throws IOException {
+        orderBackup.getWriter().append("New order: ");
+    }
+
+    @AfterEach
+    void appendAtTheEndOfTheLine() throws IOException {
+        orderBackup.getWriter().append(" backed up.");
+    }
+
+    @AfterAll
+    static void turnDown() throws IOException {
+        orderBackup.closeFile();
+    }
+
+    @Test
+    void backupOrderWithOneMeal() throws IOException {
+
+        //given
+        Meal meal = new Meal(7, "Fries");
+        Order order = new Order();
+        order.addMealToOrder(meal);
+
+        //when
+        orderBackup.backupOrder(order);
+    }
+
+}
